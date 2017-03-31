3addf34b2c84974dc5ff236cbca7a54935c52406
YassineR <yassine.rachedi@yahoo.fr>
2017-03-31 17:20:35 +0200

    Update fichier.js

--- js/fichier.js
+++ js/fichier.js
@@ -1,4 +1,4 @@
-(document).ready(function() {
+/*(document).ready(function() {
 	$.ajax({
 		url : "http://bitcoin.mubiz.com/info",
 		dataType : "json",
@@ -9,6 +9,25 @@
 
 		success : function(data) {
 			$('#bitcoin_block_number').append(data.blocks);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+		}
+	});
+});*/
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/info",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.height);
 		},
 
 		error : function(xhr, status, err) {
